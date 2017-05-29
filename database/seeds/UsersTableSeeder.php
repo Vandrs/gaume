0a0ec573a88b2f2d0305e2a3340afe60bb5fc486
Vanderson Nunes <vann.nunes@gmail.com>
2017-05-28 22:08:03 -0300

    Ajuste migrations Usuários

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
     	$qtd = $this->command->ask("Quantos usuários deseja criar?", 1);   
-    	$role = $this->command->ask("Qual perfil deseja criar ADMIN(1) STUDENT(2) TEACHER(3) ?",1);
+    	$role = $this->command->ask("Qual perfil deseja criar ADMIN(1) TEACHER(2) STUDENT(3) ?", 1);
         $users = factory(\App\Models\User::class, (int) $qtd)->create(['role_id' => (int) $role]);
         $seeder = $this;
         $this->command->info($users->count()." Usuários criados");
