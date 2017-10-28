3eed458a11258db130a4f226a4ac7c1c9768a17c
Vanderson Nunes <vann.nunes@gmail.com>
2017-10-28 12:39:30 -0200

    Ajuste pagseguro

--- app/Services/PagSeguro/CheckoutService.php
+++ app/Services/PagSeguro/CheckoutService.php
@@ -39,7 +39,7 @@ class CheckoutService
 						'group' => 'CREDIT_CARD,BOLETO'
 					]
 				]
-			],
+			]/*,
 			'paymentMethodConfigs' => [
 				[
 		            'paymentMethod' => [
@@ -52,7 +52,7 @@ class CheckoutService
 		                ]
 		            ]
 		        ]
-			]
+			]*/
 		];
 	}
 
